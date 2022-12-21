6c5670cee31e800f2501a35bdc58776ad54d7b2b
Eric Bello <ebello219@gmail.com>
2022-12-21 15:03:05 -0500

    updated api endpoint

--- components/VisitorCounter.js
+++ components/VisitorCounter.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 // import { set } from "cypress/types/lodash";
 
 export default function VisitorCounter() {
-    const COUNT_API = `https://6gl89wftdk.execute-api.us-east-1.amazonaws.com/Prod`;
+    const COUNT_API = `https://6gl89wftdk.execute-api.us-east-1.amazonaws.com/Prod/counter`;
 
     const [count, setCount] = useState();    
 
