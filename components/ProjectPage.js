4c7c291ad34a8c3304560d7aedce7e90a209f88c
Eric Bello <ebello219@gmail.com>
2023-09-16 11:31:00 -0400

    fixed dom nesting error

--- components/ProjectPage.js
+++ components/ProjectPage.js
@@ -70,7 +70,7 @@ const ProjectPage = () => {
                     >
                     <div className="my-4">&rarr;</div>
                     <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
-                        <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                        <span className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
                         Live Demo
                     </p>
                     </a>
@@ -82,7 +82,7 @@ const ProjectPage = () => {
                     >
                     <div className="my-4">&rarr;</div>
                     <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
-                        <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                        <span className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
                         GitHub Repo
                     </p>
                     </a>
