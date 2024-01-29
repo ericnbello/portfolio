a72388aa7f40c46ab8f1ad57f6eae1148dc11b0a
Eric Bello <ebello219@gmail.com>
2024-01-29 18:08:34 -0500

    added resume page with styling

---
+++ pages/resume.js
@@ -0,0 +1,11 @@
+import Navbar from '@components/Navbar.js';
+import Resume from '../components/Resume.js';
+import ContainerBlock from '@components/ContainerBlock.js';
+
+export default function resume() {
+  return (
+    <ContainerBlock>
+      <Resume />
+    </ContainerBlock>
+  )
+}
