c512aab9b0d43af253e2d8378a0e0d9f0c9bb38a
Eric Bello <ebello219@gmail.com>
2024-06-15 20:11:28 -0400

    removed duplicate logo

--- constants/data.js
+++ constants/data.js
@@ -297,10 +297,6 @@ const userData = {
       imgUrl: `https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png`
     },
     {
-      name: `Git`,
-      imgUrl: `https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png`
-    },
-    {
       name: `Firebase`,
       imgUrl: `https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png`
     }
