e30502d67a2027296da8365fefab0fb7e3b02043
Eric Bello <inviter.headset0n@icloud.com>
2024-09-27 14:12:35 -0400

    added google analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
+
 function MyApp({ Component, pageProps }) {
+
+  // <!-- Google tag (gtag.js) -->
+    <>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-HL6XJV7L25"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HL6XJV7L25');
+  </script>
+  </>
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
