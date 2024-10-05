e5f5ca9972c8faff0d369b5631e06317ba9c34fc
Eric Bello <inviter.headset0n@icloud.com>
2024-10-05 16:35:21 -0400

    removed visitor counter

--- components/VisitorCounter.js
+++ components/VisitorCounter.js
@@ -9,17 +9,17 @@ export default function VisitorCounter() {
 
     useEffect(() => {
         async function getCount() {
-            const response = await fetch('https://am522mjjnh.execute-api.us-east-1.amazonaws.com/Prod/counter');
-            const data = await response.json();
-            console.log(data)
-            setCount(data);
+            // const response = await fetch('https://am522mjjnh.execute-api.us-east-1.amazonaws.com/Prod/counter');
+            // const data = await response.json();
+            // console.log(data)
+            // setCount(data);
         }
         getCount();
     }, []);
 
     return (
         <div className="border-1 outline px-4">
-            <p>Views: {count}</p>
+            {/* <p>Views: {count}</p> */}
         </div>
     )
 }
